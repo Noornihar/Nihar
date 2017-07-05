fbad763320ce50cd4d9b973974fc62491f496da2
Noornihar <niharniluajn@gmail.com>
2017-07-05 09:31:34 +0530

    Create Nat.java

---
+++ Nat.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class Nat
+{
+public static void main(String args[])
+{
+int sum=0;
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+for(int i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+System.out.println(sum);
+}}
