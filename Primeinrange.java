06c6a3a4ec78dbc0a51fc0dd5758227eb60504f5
Noornihar <niharniluajn@gmail.com>
2017-07-06 09:32:08 +0530

    Create Primeinrange.java

---
+++ Primeinrange.java
@@ -0,0 +1,38 @@
+import java.io.*;
+ import java.util.*;
+ import java.lang.*;
+ public class Primeinrange
+{
+  public static void main(String args[]) throws ArithmeticException
+  {
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int count=0,flag=0;
+    for(int i=a;i<b;i++)
+    {
+      for(int j=2;j<i;j++)
+      {
+  
+        if(i%j==0)
+      {
+          count=0;
+        break;
+      }
+  
+      else
+      {
+          
+        count=1;
+        }
+        }
+    if(count==1)
+      {
+      
+        //System.out.println(i);
+        flag++;
+      }
+     }
+     System.out.println(flag);
+  }
+}
